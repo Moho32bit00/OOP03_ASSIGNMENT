6369e82e9841b151c3a940a7b08fec327cab642f
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-23 13:53:07 +0300

    Part 02 — Practical 1 Update the Shipment Class

---
+++ OOP03_ASSIGNMENT/DeliveryAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03_ASSIGNMENT
+{
+    public struct DeliveryAddress
+    {
+        public string City { set; get; }
+        public string Street { set; get; }
+        public int BuildingNumber { set; get; }
+
+        public DeliveryAddress(string city, string Street, int buildingNumber)
+        {
+            this.City = city;
+            this.Street = Street;
+            this.BuildingNumber = buildingNumber;
+        }
+
+        public string GetFullAddress()
+        {
+            return $"City : {City}\nStreet : {Street}\nBuilding Number : {BuildingNumber}\n";
+        }
+
+    }
+}
---
+++ OOP03_ASSIGNMENT/Shipment.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03_ASSIGNMENT
+{
+    public class Shipment
+    {
+            private string trackingCode;
+            private string description;
+            private decimal weight;
+            private decimal deliveryFee;
+
+            public string TrackingCode => trackingCode;
+
+            public string Description
+            {
+                get
+                {
+                    return description;
+                }
+                set
+                {
+                    if (value != null && value != "" && value != " ")
+                    {
+                        description = value;
+                    }
+                }
+            }
+
+            public decimal Weight
+            {
+                get { return weight; }
+
+                set
+                {
+                    if (value > 0)
+                    {
+                        weight = value;
+                    }
+                }
+            }
+
+            public decimal DeliveryFee
+            {
+                get { return deliveryFee; }
+
+                private set
+                {
+                    if (value > 0)
+                    {
+                        deliveryFee = value;
+                    }
+                }
+            }
+
+            public virtual decimal EstimatedCost { get { return DeliveryFee; } }
+
+            public DeliveryAddress Destination { get; set; }
+           //============
+           //constructors
+            public Shipment() { }
+            public Shipment(string trackingCode)
+            {
+                this.trackingCode = trackingCode;
+                Description = "Unknown";
+                Weight = 1;
+                DeliveryFee = 50;
+                Destination = new DeliveryAddress("Unknown", "Unknown", 0);
+            }
+            public Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+            {
+                this.trackingCode = trackingCode;
+                this.description = description;
+                this.weight = weight;
+                this.deliveryFee = deliveryFee;
+                Destination = destination;
+            }
+           //==========
+
+           //methods
+            public void UpdateDeliveryFee(decimal newFee)
+            {
+                if (newFee > 0)
+                {
+                    deliveryFee = newFee;
+                }
+            }
+
+
+        public virtual void PrintShipment() { }
+            
+        public void weight_update(decimal new_weight) 
+        {
+            if (new_weight > 0)
+            {
+                 weight += new_weight;
+            }
+        }
+        public void weight_update(decimal new_weight , decimal extra_pack_weight)
+        {
+            if (new_weight > 0&& extra_pack_weight> 0 )
+            {
+                weight += new_weight + extra_pack_weight;
+            }
+        }
+
+
+    }
+    
+}
