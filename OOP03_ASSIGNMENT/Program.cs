3bc0f2087463675f19c136fe6f1f3264d9bf34fe
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-23 13:16:27 +0300

    Add project files.

---
+++ OOP03_ASSIGNMENT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP03_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
