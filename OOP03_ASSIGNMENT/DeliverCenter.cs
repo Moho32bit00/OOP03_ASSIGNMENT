657ebc5614d97e892ddd3c8c55ee7f12380583cb
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-23 14:41:36 +0300

    6 Update DeliveryCenter

---
+++ OOP03_ASSIGNMENT/DeliverCenter.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03_ASSIGNMENT
+{
+    public class DeliveryCenter
+    {
+
+        private Shipment[] shipment;
+        public string CenterName { get; set; }
+        public DeliveryCenter() { this.shipment = new Shipment[20]; }
+
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < shipment.Length)
+                {
+                    return shipment[index];
+                }
+                else { return default; ; }
+            }
+            set
+            {
+                if (index >= 0 && index < shipment.Length)
+                {
+                    shipment[index] = value;
+                }
+            }
+        }
+
+        public Shipment this[string TrackingCode]
+        {
+            get
+            {
+                for (int i = 0; i < shipment.Length; i++)
+                {
+                    if (shipment[i].TrackingCode == TrackingCode)
+                    {
+                        return shipment[i];
+                    }
+
+                }
+                return default;
+            }
+        }
+
+        public Deriver Deriver { set; get; }
+
+        public bool AddShipment(Shipment s_shipment)
+        {
+            if (shipment == null) shipment = new Shipment[20];
+
+            for (int i = 0; i < shipment.Length; i++)
+            {
+                if (shipment[i] == null)
+                {
+                    shipment[i] = s_shipment;
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public bool RemoveShipment(string tracking_code)
+        {
+            for (int i = 0; i < shipment.Length; i++)
+            {
+                if (shipment[i].TrackingCode == tracking_code)
+                {
+                    for (int j = i; j < shipment.Length - 1; j++)
+                    {
+                        shipment[j] = shipment[j + 1];
+                    }
+                    shipment[shipment.Length - 1] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void PrintAllShipments()
+        {
+            foreach (var item in shipment)
+            {
+                if (item != null)
+                {
+                    item.PrintShipment();
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ OOP03_ASSIGNMENT/Deriver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03_ASSIGNMENT
+{
+    public class Deriver
+    {
+        public Deriver() { }
+        public Deriver(int driverId, string fullName, double phoneNumber)
+        {
+            DriverId = driverId;
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+        }
+
+        public int DriverId {  get; set; }
+       public string FullName {  get; set; }
+       public double PhoneNumber {  get; set; }
+
+        
+    }
+}
